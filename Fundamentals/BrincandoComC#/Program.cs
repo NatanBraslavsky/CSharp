07a500cf0a0da6ccc32d277b941c369ae6591f78
Natan Braslavsky <natanbraslavsky1@gmail.com>
2025-11-05 16:49:09 -0300

    Novo repositório para estudos em C#

---
+++ Fundamentals/BrincandoComC#/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite a quantidade de Funcionários que deseja cadastrar: ");
+int quantidadeFuncionario = int.Parse(Console.ReadLine());
+
+string[] funcionarios = new string[quantidadeFuncionario];
+
+for(int i = 0; i < quantidadeFuncionario; i++){
+    Console.Write($"Digite o {i+1}º Funcionário: ");
+    funcionarios[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\nLista de Funcionários...\n");
+for(int i = 0; i < funcionarios.Length; i++){
+    Console.WriteLine($"-{funcionarios[i]}");
+}
+
