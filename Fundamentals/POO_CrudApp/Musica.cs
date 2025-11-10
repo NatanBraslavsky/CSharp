792a1b55e5f1dfe960ef13939f9fefc3a3970b17
Natan Braslavsky <natanbraslavsky1@gmail.com>
2025-11-10 16:29:45 -0300

    feat: inicio estudo POO

--- Fundamentals/POO_CrudApp/Musica.cs
+++ Fundamentals/POO_CrudApp/Musica.cs
@@ -1,1 +1,21 @@
-﻿
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível nas plataformas de streaming.");
+        }
+        else
+        {
+            Console.WriteLine("Indisponível nas plataformas de streaming.");
+        }
+    }
+}
--- Fundamentals/POO_CrudApp/Program.cs
+++ Fundamentals/POO_CrudApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Musica Teste";
+musica1.artista = "Artista Teste";
+musica1.duracao = 300;
+musica1.disponivel = true;
+
+musica1.ExibirFichaTecnica();
