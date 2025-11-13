60ba36b8c309e3f1f5cac29e4ded4196e2fd8764
Natan Braslavsky <natanbraslavsky1@gmail.com>
2025-11-12 21:28:35 -0300

    estudos c#

---
+++ Fundamentals/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> lista = new List<int> { 1, 5, 3, 2, 6, 8 };
+
+//List<int> listaPares = lista.FindAll(Par);
+
+//Assim
+
+//bool Par(int numero)
+//{
+//    return numero % 2 == 0;
+//}
+
+//foreach(int numeros in listaPares)
+//{
+//    Console.WriteLine(numeros);
+//}
+
+
+//Ou assim
+
+List<int> listaPar = lista.FindAll(numero => numero % 2 == 0);
+foreach(int numero in listaPar)
+{
+    Console.WriteLine(numero);
+}
