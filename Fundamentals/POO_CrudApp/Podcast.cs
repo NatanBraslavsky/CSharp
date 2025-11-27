647c7d34951f5b44d74b8306db8d5bf5d184d262
Natan Braslavsky <natanbraslavsky1@gmail.com>
2025-11-27 08:41:52 -0300

    Exercicios C#

---
+++ Fundamentals/POO_CrudApp/Podcast.cs
@@ -0,0 +1,11 @@
+﻿class Podcast
+{
+    public Podcast(int host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+    public int Host { get; set; }
+    public string Nome { get; set; }
+    
+}
